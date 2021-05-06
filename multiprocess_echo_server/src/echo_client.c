b6b592a82b3653fb5894e5d17e2b33206619fe9f
bishil06 <bishil06@naver.com>
2021-05-06 18:44:41 +0900

    Feat echo_server, echo_clent
    echo_server, echo_client 추가

---
+++ multiprocess_echo_server/src/echo_client.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024
+
+void error_handling(char *msg) {
+    fprintf(stderr, "%s \n", msg);
+    exit(1);
+}
+
+// 부모프로세스 담당
+void read_routine(int sock, char *buf) {
+    while (true) {
+        int str_len = read(sock, buf, BUF_SIZE);
+        if (str_len == 0) return;
+
+        buf[str_len] = 0;
+        printf("from server: %s\n", buf);
+    }
+}
+
+// 자식 프로세스 담당
+void write_routine(int sock, char *buf) {
+    while (true) {
+        fgets(buf, BUF_SIZE, stdin); // 사용자 입력스트림을 가져온닽
+        if(!strcmp(buf,"q\n") || !strcmp(buf,"Q\n")) {
+            shutdown(sock, SHUT_WR);
+            return;
+        }
+        write(sock, buf, strlen(buf));
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if(argc!=3) {
+		printf("how to use : %s <ip> <port>\n", argv[0]);
+		exit(1);
+	}
+
+    // 소켓 생성
+    int sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        error_handling("socket() error");
+    }
+
+    // 소켓 연결
+    struct sockaddr_in serv_addr = { 0, };
+    serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
+	serv_addr.sin_port=htons(atoi(argv[2]));
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+        error_handling("connect() error");
+    }
+
+    pid_t pid = fork();
+    char buf[BUF_SIZE] = { 0, };
+    if (pid == 0) {
+        write_routine(sock, buf);
+    }
+    else {
+        read_routine(sock, buf);
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ multiprocess_echo_server/src/echo_server.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define BUF_SIZE 1024
+
+void error_handling(char *msg) {
+    fprintf(stderr, "%s \n", msg);
+    exit(1);
+}
+
+void endChildAction(int sig) {
+    int status = 0;
+    pid_t pid = waitpid(-1, &status, WNOHANG);
+    if (WIFEXITED(status)) {
+        printf("pid = %d child process success\n", pid);
+    }
+    else {
+        printf("pid = %d child process fail\n", pid);
+    }
+    printf("child process return %d\n", WEXITSTATUS(status));
+}
+
+int main(int argc, char *argv[]) {
+    // 자식프로세스 처리 시그널 추가
+    struct sigaction act = { 0, };
+    act.sa_handler = endChildAction;
+    act.sa_flags = 0;
+    sigemptyset(&act.sa_mask);
+
+    if (argc != 2) {
+        printf("how to use: %s port\n", argv[0]);
+        exit(1);
+    }
+
+    // 소켓 생성
+    int serv_sock = -1;
+    serv_sock=socket(PF_INET, SOCK_STREAM, 0);
+    if (serv_sock == -1) {
+        error_handling("socket() error");
+    }
+    // 소켓 주소 설정
+    struct sockaddr_in serv_addr = { 0, };
+    serv_addr.sin_family=AF_INET;
+	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+	serv_addr.sin_port=htons(atoi(argv[1]));
+    if (bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) {
+        error_handling("bind() error");
+    }
+
+    if (listen(serv_sock, 5) == -1) {
+        error_handling("listen() error");
+    }
+
+    while (true) {
+        struct sockaddr_in clnt_addr = { 0, };
+        socklen_t clnt_addr_size = sizeof(clnt_addr);
+        int clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+        if (clnt_sock == -1) {
+            fprintf(stderr, "accpet() error\n");
+            continue;
+        }
+        else {
+            printf("new client connected ...\n");
+        }
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            close(clnt_sock);
+            continue;
+        }
+        if (pid == 0) {
+            close(serv_sock);
+            int str_len = 0;
+            char buf[BUF_SIZE] = { 0, };
+            while (true) {
+                str_len = read(clnt_sock, buf, BUF_SIZE);
+                if (str_len == 0) {
+                    break;
+                }
+
+                write(clnt_sock, buf, str_len);
+            }
+            close(clnt_sock);
+            printf("client disconnected ...\n");
+            return 0;
+        }
+    }
+    
+    return 0;
+}
